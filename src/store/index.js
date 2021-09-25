f4ff6a4db0615add3884f4db577a8cfbda66fb2c
Alexis Torres <torres.alexis04@yahoo.com>
2021-09-24 18:13:39 -0700

    Store content
    Redux now stores content of the current page

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware } from "redux";
 import loggerMiddleware from "redux-logger";
 import ThunkMiddleware from "redux-thunk";
-import { joinCampaign } from "./utils/reducerFunction";
+import { joinCampaign, updatePage } from "./utils/reducerFunction";
 
 const JOIN_CAMPAIGN = "JOIN_CAMPAIGN";
+const UPDATE_PAGE = "UPDATE_PAGE";
+
+export const updatePageData = (pageData) => {
+  return {
+    type: UPDATE_PAGE,
+    pageData,
+  }
+}
 
 export const joinNewCampaign = (campaignName) => {
   return {
@@ -15,9 +23,9 @@ export const joinNewCampaign = (campaignName) => {
 const initialTestState = {
   user: {id: 1, userName: "tishara", email:"topazdragon20@yahoo.com"},
   currentPage: {
-    title:"Therai", 
-    campaign:"Avatar The Last Airbender My World", 
+    campaignName:"Avatar The Last Airbender My World", 
     jumps:["Summary", "Backstory", "Campaign Start", "Death"], 
+    content: {},
     info:{City: "Human Town", Parents: "Unknown", Class: "Gambler", Gender: "Male", Race: "Tiefling", Age: 28, Description: "Thin build, maroon Tiefling with shaved down horns. Wears a colorshifting cloak over a more ornate street attire"}},
   campaigns: [
     {id:1, campaignName: "Avatar The Last Airbender My World", pages:["Water Benders", "Fire Benders", "Earth Benders", "Air Benders", "Ashleys Pengu", "Oscars character", "Addy's Character", "Firebending Librarians"], members:["Alexis"]}, 
@@ -28,6 +36,8 @@ const appReducer = (state = initialTestState, action) => {
   switch (action.type) {
     case JOIN_CAMPAIGN:
       return joinCampaign(state, action.campaignName);
+    case UPDATE_PAGE:
+      return updatePage(state, action.pageData)
     default:
       return state
   }
--- src/store/utils/reducerFunction.js
+++ src/store/utils/reducerFunction.js
@@ -2,3 +2,9 @@
 export const joinCampaign = (state, action) => {
   return action.campaignName;
 }
+
+export const  updatePage = (state, pageData) => {
+  const newState = {...state};
+  newState.currentPage.content = pageData;
+  return newState;
+}
